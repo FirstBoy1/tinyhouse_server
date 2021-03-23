37e18cb58317bd0c1add44e234daae3554aec34b
Firstboy <zeems1234@gmail.com>
2021-03-24 00:34:46 +0500

    Added user listing and booking section in user page

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,5 +1,12 @@
 import merge from 'lodash.merge'
 import { viewerResolvers } from './resolvers/Viewer'
 import { userResolvers } from './resolvers/User'
+import { listingResolvers } from './resolvers/Listing'
+import { bookingResolvers } from './resolvers/Booking'
 
-export const resolvers = merge(viewerResolvers, userResolvers)
+export const resolvers = merge(
+  viewerResolvers,
+  userResolvers,
+  listingResolvers,
+  bookingResolvers
+)
